daf48493b5282c98f67f03635244a36c6878db44
shrinath45 <shrinathadhav45@gmail.com>
2025-01-20 10:29:01 +0530

    modified

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log('Welcome to Typescript');
+
+let product:{name:string, price:number, stock:boolean, rating?:{rate:number, count:number}} = {
+    name: "TV",
+    price: 34000.50,
+    stock: true,
+    rating: {rate:4.5, count:45}
+
+}
